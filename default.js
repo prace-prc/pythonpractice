31d201ce23f7128b23b1ea1ff78a8847bbef63df
Juwon Hwang <wndnjs719@gmail.com>
2023-05-30 13:15:21 +0900

    변수 실습

--- default.js
+++ default.js
@@ -1,12 +1,37 @@
-document.write("Hello World");
-document.write("<h1>Welcome to JS Program</h1>")
-document.write("<h2>Welcome to JS Program</h2>")
+// document.write("Hello World");
+// document.write("<h1>Welcome to JS Program</h1>")
+// document.write("<h2>Welcome to JS Program</h2>")
 
-console.log('Welcome JS Program');
-console.info('Welcome JS Program');
-console.warn('Welcome JS Program');
-console.error('Welcome JS Program');
+// console.log('Welcome JS Program');
+// console.info('Welcome JS Program');
+// console.warn('Welcome JS Program');
+// console.error('Welcome JS Program');
 
-alert('Welcome JS Program');
-var a = prompt('Welcome JS Program');
-console.log(a);
+// alert('Welcome JS Program');
+// var a = prompt('Welcome JS Program');
+// console.log(a);
+
+console.log(123, typeof 123);
+console.log(123.5, typeof 123.5);
+console.log("123", typeof "123");
+console.log(true, typeof true);
+console.log(false, typeof false);
+
+var car
+console.log(car);
+var car = ""
+console.log(typeof car);
+
+var person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue"};
+console.log(typeof person, person);
+person = null;
+console.log(typeof person, person);
+
+var name = "이승훈";
+var age = 29;
+var cgpa = 3.92
+var lineBreak = "<br/>"
+
+document.write("이름: " + name + lineBreak);
+document.write("나이: " + age + lineBreak);
+document.write("학점: " + cgpa + lineBreak);
