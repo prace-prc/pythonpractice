2f32bafb6c15d2356cab9b7d89165f61cf304f7a
JM Bae <jungman82@gmail.com>
2023-05-30 13:02:56 +0900

    데이터 타입과 주석

--- default.js
+++ default.js
@@ -1,12 +1,27 @@
-document.write("Hello World");
-document.write("<h1>Welcome to JS Program</h1>");
-document.write("<h2>Welcome to JS Program</h2>")
 
-console.log('Welcome JS Program');
-console.info('Welcome JS Program');
-console.warn('Welcome JS Program');
-console.error('Welcome JS Program');
+// console.log(123, typeof 123);
+// console.log(123.5, typeof 123.5);
+// console.log("123", typeof "123");
+// console.log(true, typeof true);
+// console.log(false, typeof false);
 
-alert('Welcome JS Program');
-var a = prompt('Welcome JS Program');
-console.log(a);
+var car;
+console.log(car);
+var car = "";
+console.log(typeof car);
+console.log();
+var person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue"};
+console.log(typeof person, person);
+person = null;
+console.log(typeof person, person);
+
+// document.write("Hello World");
+// document.write("<h1>Welcome to JS Program</h1>");
+// document.write("<h2>Welcome to JS Program</h2>");
+//
+// console.log('Welcome JS Program');
+// console.info('Welcome JS Program');
+// console.warn('Welcome JS Program');
+// console.error('Welcome JS Program');
+//
+// alert('Welcome JS Program');
