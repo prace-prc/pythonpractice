636561f85011b8a7fd26c2d10e0916fbff867fa8
Juwon Hwang <wndnjs719@gmail.com>
2023-05-31 11:07:06 +0900

    모음 자음 구별하기 실습

--- default.js
+++ default.js
@@ -128,49 +128,60 @@
 //
 // document.write("화씨 온도: " + farn);
 
-var num1 = 20;
-var num2 = 10;
-var num3 = "10";
-var num4 = 20;
-var num5 = 15;
+// var num1 = 20;
+// var num2 = 10;
+// var num3 = "10";
+// var num4 = 20;
+// var num5 = 15;
+//
+// console.log("일반 크기 비교");
+// console.log(num1 > num2, num1, '>', num2);
+// console.log(num1 >= num2, num1, '>=', num2);
+// console.log(num1 < num2, num1, '<', num2);
+// console.log(num1 <= num2, num1, '<=', num2);
+//
+// console.log("같은지 여부 확인");
+// console.log(num1 == num4, num1, '==', num4);
+// console.log(num1 != num4, num1, '!=', num4);
+//
+// console.log("===");
+// console.log(num1 === num3, num1, '===', num3);
+// console.log(num2 === num3, num2, '===', num3);
+// console.log(num2 == num3, num2, '==', num3);
+//
+// console.log('num1 > num2 && num1 < num5',num1 > num2 && !(num1 < num5));
+// console.log('num1 > num2 || num1 < num5',num1 > num2 || num1 < num5);
+//
+// var num1 = parseInt(prompt('첫번째 숫자 입력: '));
+// var num2 = parseInt(prompt('두번째 숫자 입력: '));
+//
+// if(num1 > num2) {
+//  console.log("큰 수는 num1 : ", num1);
+// }
+//
+// if(num1 < num2) {
+//  console.log("큰 수는 num2 : ", num2);
+// }
+//
+// if(num1 == num2){
+//  console.log("같은 수");
+// }
+//
+// if(num1 > num2){
+//  console.log("큰 수는 num1 : ", num1);
+// } else if (num1 < num2) {
+//  console.log("큰 수는 num2 : ", num2);
+// } else {
+//  console.log("같은 수");
+// }
 
-console.log("일반 크기 비교");
-console.log(num1 > num2, num1, '>', num2);
-console.log(num1 >= num2, num1, '>=', num2);
-console.log(num1 < num2, num1, '<', num2);
-console.log(num1 <= num2, num1, '<=', num2);
+var letter = prompt("Enter a letter : ");
 
-console.log("같은지 여부 확인");
-console.log(num1 == num4, num1, '==', num4);
-console.log(num1 != num4, num1, '!=', num4);
+letter = letter.toLowerCase();
 
-console.log("===");
-console.log(num1 === num3, num1, '===', num3);
-console.log(num2 === num3, num2, '===', num3);
-console.log(num2 == num3, num2, '==', num3);
-
-console.log('num1 > num2 && num1 < num5',num1 > num2 && !(num1 < num5));
-console.log('num1 > num2 || num1 < num5',num1 > num2 || num1 < num5);
-
-var num1 = parseInt(prompt('첫번째 숫자 입력: '));
-var num2 = parseInt(prompt('두번째 숫자 입력: '));
-
-if(num1 > num2) {
- console.log("큰 수는 num1 : ", num1);
-}
-
-if(num1 < num2) {
- console.log("큰 수는 num2 : ", num2);
-}
-
-if(num1 == num2){
- console.log("같은 수");
-}
-
-if(num1 > num2){
- console.log("큰 수는 num1 : ", num1);
-} else if (num1 < num2) {
- console.log("큰 수는 num2 : ", num2);
+if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'){
+ console.log('Vowel');
 } else {
- console.log("같은 수");
+ console.log('Consonant');
 }
+
