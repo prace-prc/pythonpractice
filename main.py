13962a3ab7a8a8c942648ce628bc023537112f02
JM Bae <jungman82@gmail.com>
2023-06-05 15:23:06 +0900

    출력 함수 실습

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+data = [10, 20, -50, 21.3, 'LikeLion']
+print(data)
 
-print("1")
-print("2", end='')
-print("3")
-print("4")
+print("Like", "Share", "Subscribe", sep='')
+print("Like", "Share", "Subscribe", sep='***')
+
+print("Like", "Share", "Subscribe", sep='***', end='\t')
+print("Like", "Share", "Subscribe", sep='***', end='\n')
+
+
+m = 40
+print("value: ", m)
+
+name = "배정만"
+age = 42
+print("My name is", name, "and My age is", age, sep=' ')
+
+print("Welcome", end='\t')
+print("to", end='\t')
+print("LikeLion")
