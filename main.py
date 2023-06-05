a5067d35d32a596a27f7787dcf2810496f93cdd7
JM Bae <jungman82@gmail.com>
2023-06-05 13:46:58 +0900

    is 연산자  실습

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-# 멤버 in 연산자
+# is 연산자
 
-st1 = "Welcome to 멋쟁이 사자"
-print("to" not in st1)
+a = 10
+b = 10
+print(a is not b)
 
-st2 = "Welcome top 멋쟁이 사자"
-print("to" not in st2)
-
-st3 = "Welcome to 멋쟁이 사자"
-print("subs" not in st3)
+a = 10
+b = '10'
+print(a is not b)
