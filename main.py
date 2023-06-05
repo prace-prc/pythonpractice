5e0d7a0fae5456c02da4d11b5f905eba232f3398
JM Bae <jungman82@gmail.com>
2023-06-05 12:51:00 +0900

    비교 연산자 실습(작업중)

--- main.py
+++ main.py
@@ -1,35 +1,14 @@
-# 덧셈
-a = 4
-b = 2
-total = a + b
-print('total = a + b:', total)
+print('Hello, World!')
 
-#뺄셈
-a = 4
-b = 2
-total = a - b
-print('total = a - b: ', total)
-
-#곱셈
-total = a * b
-print('total = a * b: ', total)
-
-#나눗셈
-print('a / b: ', a / b)
-
-#나머지
+# 비교 연산자
 a = 5
 b = 2
-print('a % b: ', a % b)
+print('a:', a, 'b:', b)
 
-#거듭제곱
-print('a ** b: ', a ** b)
+print('a < b: ', a < b)
 
-# 몫 (양수)
-print('a // b : ', a // b)
+print('a <= b: ', a <= b)
 
-# 몫 (음수)
-a = -5
-print('a // b(음수) :', a // b)
+print('a == b: ', a == b)
 
-
+print('a != b: ', a != b)
