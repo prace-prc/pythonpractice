0ba561eb8d1a8a180ac276f1590d08e29696af31
JM Bae <jungman82@gmail.com>
2023-06-07 10:31:35 +0900

    for in 실습

--- main.py
+++ main.py
@@ -1,26 +1,8 @@
-for i in range(5):
-    print(i)
+# for in
+st = "멋쟁이 사자"
+for ch in st:
+    print(ch)
+else:
+    print("Else")
 
-for i in range(2,7):
-    print(i)
-
-for i in range(1, 10, 2):
-    print(i)
-
-for i in range(-1, -10, -2):
-    print(i)
-
-a = range(5)
-print(a[0])
-print(a[1])
-print(a[2])
-print(a[3])
-print(a[4])
-
-print("Reverse Rage with Start, stop, step")
-r = range(5, 0, -1)
-print(r[0])
-print(r[1])
-print(r[2])
-print(r[3])
-print(r[4])
+print("코드 종료")
