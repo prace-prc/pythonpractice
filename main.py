62c0f5bcadc731697993abe3cce618375e12c831
Juwon Hwang <wndnjs719@gmail.com>
2023-06-07 10:19:34 +0900

    while문 실습

--- main.py
+++ main.py
@@ -221,57 +221,116 @@
 # print('He said, "Hello World"')
 # print('It\'s beautiful day')
 # print("It's beautiful day")
+#
+# # 간단한 if문
+# x = 5
+# if x > 3:
+#     print("x는 3보다 큽니다.")
+#
+# # if else
+# age = 18
+# if age >= 18:
+#     print("성인입니다.")
+# else:
+#     print("미성년자입니다.")
+#
+# # 중첩된 if else
+# score = 85
+# if score >= 90:
+#     print("A 등급")
+# else:
+#     if score >= 80:
+#         print("B 등급")
+#     else:
+#         if score >= 70:
+#             print("C 등급")
+#         else:
+#             print("D 등급")
+#
+# # if elif
+# marks = 75
+# if marks >= 90:
+#     print("A등급")
+# elif marks >= 80:
+#     print("B등급")
+# elif marks >= 70:
+#     print("C등급")
+# else:
+#     print("D등급")
+#
+# # 입력 if
+# a = int(input("Enter Number Greater then or equal to 2: "))
+# if a >= 2:
+#     print("Correct! You have Entered :", a)
+# else:
+#     print("Wrong! You have Entered :", a)
+#
+# #input에 대한 조건문 else
+# day = input("요일을 입력하세요: ")
+# if day == "Mon":
+#     print("월요일")
+# elif day == "Tue":
+#     print("화요일")
+# elif day == "Wed":
+#     print("수요일")
+# else:
+#     print("휴일")
 
-# 간단한 if문
-x = 5
-if x > 3:
-    print("x는 3보다 큽니다.")
-
-# if else
-age = 18
-if age >= 18:
-    print("성인입니다.")
+# while문
+i = int(input("Enter Number :"))
+while i < 10:
+    i += 1
+    print("i: ", i)
 else:
-    print("미성년자입니다.")
+    print("else")
 
-# 중첩된 if else
-score = 85
-if score >= 90:
-    print("A 등급")
-else:
-    if score >= 80:
-        print("B 등급")
-    else:
-        if score >= 70:
-            print("C 등급")
-        else:
-            print("D 등급")
+# 끝나지 않는 while문
+while True:
+    a = int(input("Enter Menu Number: "))
+    if a == 0:
+        break
+    print("a: ", a)
 
-# if elif
-marks = 75
-if marks >= 90:
-    print("A등급")
-elif marks >= 80:
-    print("B등급")
-elif marks >= 70:
-    print("C등급")
-else:
-    print("D등급")
+a = 1
+while a <= 10:
+    print(a)
+    a += 1
+print("코드 종료")
 
-# 입력 if
-a = int(input("Enter Number Greater then or equal to 2: "))
-if a >= 2:
-    print("Correct! You have Entered :", a)
-else:
-    print("Wrong! You have Entered :", a)
+a = 2
+while a <= 20:
+    print(a)
+    a += 2
+print("코드 종료")
 
-#input에 대한 조건문 else
-day = input("요일을 입력하세요: ")
-if day == "Mon":
-    print("월요일")
-elif day == "Tue":
-    print("화요일")
-elif day == "Wed":
-    print("수요일")
-else:
-    print("휴일")
+a = 1
+while a <= 10:
+    print(a)
+    a += 1
+else: print("While 조건이 거짓이므로 Else 부분 실행됨")
+print("코드 종료")
+
+# # 무한 루프
+# while True:
+#     print("안녕하세요")
+# print("코드 종료")
+
+i = 0
+while True:
+    i += 1
+    print(i)
+    if i == 5:
+        break
+print("코드 종료")
+
+# 중첩
+i = 1
+while i <= 3:
+    print("Outer Loop", i)
+    i += 1
+    j = 1
+    while j <= 5:
+        print("Inner Loop", j)
+        j += 1
+print("코드 종료")
+
