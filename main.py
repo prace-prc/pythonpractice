465b85cd5f2d1917b0db9ed48e951f50340fbc93
Juwon Hwang <wndnjs719@gmail.com>
2023-06-07 11:40:30 +0900

    배열 생성, 삽입, 삭제, pop() 실습

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 # # 간단한 if문
 # x = 5
 # if x > 3:
@@ -191,9 +190,30 @@ while i < n:
     print(stu_roll[i])
     i += 1
 
-print("Array After Insert")
+print("배열 삽입 실습")
 stu_roll.insert(1, 106)
 stu_roll.insert(3, 107)
+n = len(stu_roll)
+i = 0
+while i < n:
+    print(stu_roll[i])
+    i += 1
+
+# 배열 요소 삭제
+
+print("배열 요소 삭제")
+stu_roll.remove(107)
+n = len(stu_roll)
+i = 0
+while i < n:
+    print(stu_roll[i])
+    i += 1
+
+# 배열 pop() 함수 실습
+
+print("배열 pop() 함수 실습")
+element = stu_roll.pop(2)
+print('element :',element)
 n = len(stu_roll)
 i = 0
 while i < n:
