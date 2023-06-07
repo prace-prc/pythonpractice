9d39757c0b12d8aa1a83c00a0efdff84c3117129
JM Bae <jungman82@gmail.com>
2023-06-07 09:32:06 +0900

    if문 실습

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
-name = input()
+예제 1: 간단한 if 문
+x = 5
+if x > 3:
+    print("x는 3보다 큽니다.")
 
-print(name)
+if else
+age = 18
+if age >= 18:
+    print("성인입니다.")
+else:
+    print("미성년자입니다.")
 
-name = input("Your Name: ")
-mobile = input("Enter Your Mobile Number:")
-mb = int(mobile)
-print(mb, type(mb))
+중첩된 if else
+score = 50
+if score >= 90:
+    print("A 학점")
+else:
+    if score >= 80:
+        print("B 학점")
+    else:
+        if score >= 70:
+            print("C 학점")
+        else:
+            print("D 학점")
 
-price = int(input("Total Price:"))
+예제 4: if elif
+marks = 75
+if marks >= 90:
+    print("A 등급")
+elif marks >= 80:
+    print("B 등급")
+elif marks >= 70:
+    print("C 등급")
+else:
+    print("D 등급")
 
-print(price)
-complex_number = complex(input("Enter Complex Number:"))
-print(complex_number)
+a = int(input("Enter Number Greater then or equal to 2:"))
+if a >= 2:
+    print("Correct!! You have Entered: ", a)
+else:
+    print("Wrong!! You have Entered:", a)
 
-print("He said, \"Hello World\"")
-print('He said, "Hello World"')
-print('It\'s beautiful day')
-print("It's beautiful day")
+day = input("요일을 입력하세요: ")
+if day == "Mon":
+    print("오늘은 월요일")
+elif day == "Tue":
+    print("화요일")
+elif day == 'Wed':
+    print("수요일")
+else:
+    print("휴일")
