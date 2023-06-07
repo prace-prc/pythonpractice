de6a4414725d312afbab7584765a4971f28cbb28
Juwon Hwang <wndnjs719@gmail.com>
2023-06-07 13:04:39 +0900

    배열 메소드 실습

--- main.py
+++ main.py
@@ -178,42 +178,67 @@
 # while i < n:
 #     print(stu_roll[i])
 #     i += 1
+#
+# # 배열에 삽입
+#
+# import array
+# stu_roll = array.array('i', [101,102,103,104,105])
+#
+# n = len(stu_roll)
+# i = 0
+# while i < n:
+#     print(stu_roll[i])
+#     i += 1
+#
+# print("배열 삽입 실습")
+# stu_roll.insert(1, 106)
+# stu_roll.insert(3, 107)
+# n = len(stu_roll)
+# i = 0
+# while i < n:
+#     print(stu_roll[i])
+#     i += 1
+#
+# # 배열 요소 삭제
+#
+# print("배열 요소 삭제")
+# stu_roll.remove(107)
+# n = len(stu_roll)
+# i = 0
+# while i < n:
+#     print(stu_roll[i])
+#     i += 1
+#
+# # 배열 pop() 함수 실습
+#
+# print("배열 pop() 함수 실습")
+# element = stu_roll.pop(2)
+# print('element :',element)
+# n = len(stu_roll)
+# i = 0
+# while i < n:
+#     print(stu_roll[i])
+#     i += 1
 
-# 배열에 삽입
+# 배열에서 특정 요소만 가져오기
 
-import array
-stu_roll = array.array('i', [101,102,103,104,105])
+from array import *
+stu_roll = array('i', [101,102,103,104,105])
 
+print("array Index 메소드")
+print(stu_roll.index(101))
+
+print("extend() 메소드")
+arr = array('i', [201, 208, 209])
+stu_roll.extend(arr)
 n = len(stu_roll)
 i = 0
 while i < n:
     print(stu_roll[i])
     i += 1
 
-print("배열 삽입 실습")
-stu_roll.insert(1, 106)
-stu_roll.insert(3, 107)
-n = len(stu_roll)
-i = 0
-while i < n:
-    print(stu_roll[i])
-    i += 1
-
-# 배열 요소 삭제
-
-print("배열 요소 삭제")
-stu_roll.remove(107)
-n = len(stu_roll)
-i = 0
-while i < n:
-    print(stu_roll[i])
-    i += 1
-
-# 배열 pop() 함수 실습
-
-print("배열 pop() 함수 실습")
-element = stu_roll.pop(2)
-print('element :',element)
+print("reverse() 메소드")
+stu_roll.reverse()
 n = len(stu_roll)
 i = 0
 while i < n:
