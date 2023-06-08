f419b26b96cdf8698f068d2da91cffab079323d5
Juwon Hwang <wndnjs719@gmail.com>
2023-06-08 13:25:39 +0900

    제네레이터 실습 2

--- main.py
+++ main.py
@@ -194,24 +194,38 @@
 #     print(element)
 #
 # print(user_input_list)
+#
+# # 제네레이터 실습
+#
+# def fibonacci(n):
+#     a, b = 0, 1
+#     for _ in range(n):
+#         yield a
+#         a, b = b, a + b
+#
+#
+# runner = fibonacci(10)
+#
+# print(next(runner))
+#
+# print("======")
+# print(runner)
+# print(next(runner))
+# print("======")
+#
+# for num in runner:
+#     print(num)
 
-# 제네레이터 실습
+# 알파넷 제네레이터
 
-def fibonacci(n):
-    a, b = 0, 1
-    for _ in range(n):
-        yield a
-        a, b = b, a + b
+def generate_alphabet(start_letter, end_letter):
+    start = ord(start_letter)
+    end = ord(end_letter)
+    while start <= end:
+        yield chr(start)
+        start += 1
 
+runner = generate_alphabet("A", "F")
 
-runner = fibonacci(10)
-
-print(next(runner))
-
-print("======")
-print(runner)
-print(next(runner))
-print("======")
-
-for num in runner:
-    print(num)
+for letter in runner:
+    print(letter)
