c2a7b25dff79802e8fcf46c12ded69fdacf286d3
Juwon Hwang <wndnjs719@gmail.com>
2023-06-08 11:12:02 +0900

    리스트 실습 - 기본

--- main.py
+++ main.py
@@ -97,33 +97,65 @@
 #
 # myfun()
 # print("Global Variable a: ", a)
+#
+# i = 1
+#
+# i += 1
+#
+# def myfun():
+#     b = i + 1
+#     print("My function i:", i)
+#
+# myfun()
+#
+# # Global 키워드
+#
+# a = 50
+#
+# def show():
+#     a = 10
+#     print("show-A: ", a)
+#
+# show()
+# print("A:", a)
+#
+# def show2():
+#     global a
+#     print("show2-A: ", a)
+#     a= 20
+#     print("show2-A2: ", a)
+#
+# show2()
+# print("A: ", a)
 
-i = 1
+# 리스트 실습
 
-i += 1
+fruits = ["apple", "banana", "cherry", "orange"]
 
-def myfun():
-    b = i + 1
-    print("My function i:", i)
+print(fruits)
 
-myfun()
+fruits.append("grape")
 
-# Global 키워드
+print(fruits)
 
-a = 50
+fruits.insert(2, "kiwi")
 
-def show():
-    a = 10
-    print("show-A: ", a)
+print(fruits)
 
-show()
-print("A:", a)
+print(fruits.pop())
+print(fruits.pop(1))
 
-def show2():
-    global a
-    print("show2-A: ", a)
-    a= 20
-    print("show2-A2: ", a)
+fruits.append("cherry")
+print(fruits)
 
-show2()
-print("A: ", a)
+print(fruits.index("cherry"))
+print(fruits.remove("cherry"))
+print(fruits.index("cherry"))
+print(fruits.remove("cherry"))
+# print(fruits.index("cherry"))
+
+fruits.reverse()
+
+print(fruits)
+
+
