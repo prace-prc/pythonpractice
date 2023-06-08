deb0a982699553e367dd0bd2c418900e217f33df
JM Bae <jungman82@gmail.com>
2023-06-08 13:04:58 +0900

    제네레이터 실습 1

--- main.py
+++ main.py
@@ -1,31 +1,18 @@
-fruits = ["apple", "banana", "cherry", "orange"]
-vegetables = ["carrot", "cucumber"]
-
-grocery = fruits + vegetables
-print(grocery)
-
-numbers = [10, 5, 8, 1, 7]
-numbers.sort()
-print(numbers)
-
-slice_numbers = numbers[1:4]
-print(slice_numbers)
-
-alias_number = numbers
-print(alias_number)
-
-alias_number.pop()
-
-print(numbers)
-
-numbers_copy = numbers.copy()
-numbers_copy.pop()
-print('numbers_copy', numbers_copy)
-print('numbers',numbers)
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
 
 
-numbers_clone = numbers[:]
-print(numbers_clone)
+runner = fibonacci(100)
 
+print(next(runner))
 
+print("======")
+print(runner)
+print(next(runner))
+print("======")
 
+for num in runner:
+    print(num)
