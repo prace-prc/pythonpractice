3254fcf459f6b0118d79061228aa2c3d7c90b034
Juwon Hwang <wndnjs719@gmail.com>
2023-06-08 15:30:56 +0900

    함수 실습 - Call by Value

--- main.py
+++ main.py
@@ -377,14 +377,26 @@
 # print(stu)
 #
 # print(stu.popitem())
+#
+# def val(lst):
+#     print("Inside Function Before Append:", lst, id(lst))
+#     lst.append(4)
+#     print("Inside Function Before Append:", lst, id(lst))
+#
+#
+# lst = [1, 2, 3]
+# print("Before Calling Function: ", lst, id(lst))
+# val(lst)
+# print("Before Calling Function: ", lst, id(lst))
 
-def val(lst):
-    print("Inside Function Before Append:", lst, id(lst))
-    lst.append(4)
-    print("Inside Function Before Append:", lst, id(lst))
+def val(x):
+    print("Inside :", x, id(x))
+    x += 1
+    print("Inside After:", x, id(x))
 
+print("======")
 
-lst = [1, 2, 3]
-print("Before Calling Function: ", lst, id(lst))
-val(lst)
-print("Before Calling Function: ", lst, id(lst))
+x = 10
+print("Before Calling:", x, id(x))
+val(x)
+print("After Calling: ", x, id(x))
