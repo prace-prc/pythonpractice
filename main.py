18382bbaa0239bf4a3202fc34025b7a81f14dc81
JM Bae <jungman82@gmail.com>
2023-06-08 11:12:02 +0900

    리스트 실습 1

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
-a = 50
+
+fruits = ["apple", "banana", "cherry", "orange"]
+
+print(fruits)
+
+fruits.append("grape")
+
+print(fruits)
+
+fruits.insert(2, "kiwi")
+
+print(fruits)
+
+print(fruits.pop())
+print(fruits.pop(1))
+
+print(fruits)
+
+fruits.append("cherry")
+print(fruits)
+
+print(fruits.index("cherry"))
+print(fruits.remove("cherry"))
+print(fruits.index("cherry"))
+print(fruits.remove("cherry"))
+# print(fruits.index("cherry"))
+
+fruits.reverse()
+
+print(fruits)
 
 
-def show():
-    a = 10
-    print("show-A: ", a)
-
-
-show()
-print("A:", a)
-
-
-def show2():
-    global a
-    print("show2-A: ", a)
-    a = 20
-    print("show2-A2: ", a)
-
-
-show2()
-print("A: ", a)
