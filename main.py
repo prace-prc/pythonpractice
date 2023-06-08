720c0f16129cf6f1c78f08c77e9726516c8270c0
JM Bae <jungman82@gmail.com>
2023-06-08 10:39:20 +0900

    지역 변수와 전역 변수 실습

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def add(x, **num):
-    z = x + num['a'] + num['b'] + num['c']
-    print('Addition: ', z)
+def show():
+    x = 10
+    print(x)
 
+show()
 
-add(3, a=5, b=2, c=4, d=5)
+def add(y):
+    x = 10
+    print(x)
+    print(x+y)
+
+add(20)
+
+a = 50
+def show():
+    x = 10
+    print(x) # local
+    print(a) # global
+
+show()
+
+print("Gobal Variable a:", a)
+i = 0
+def myfun():
+    a = i + 1
+    print("My Function a:", a)
+myfun()
+print("Gobal Variable a:", a)
