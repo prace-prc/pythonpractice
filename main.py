d9db8a4a74ff87c5ed49d58298b26f5803b84bc7
JM Bae <jungman82@gmail.com>
2023-06-09 14:19:31 +0900

    여러줄 쓰기 실습
    list_example.txt ignore

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-with open('example.txt', 'r') as file_object:
-    lines = file_object.readlines()
-    for line in lines:
-        print('>', line.strip())
+# import os
+#
+# filename = 'example1.txt'
+#
+# print("파일이 존재하는지 확인하기")
+# if os.path.isfile(filename):
+#     print(f"{filename}이 존재합니다.")
+# else:
+#     print(f"{filename}이 없습니다.")
+
+file_object = open('list_example.txt', 'w')
+
+content_list = ["Python", "Java", "C++", "Javascript"]
+
+for item in content_list:
+    print(file_object.tell())
+    file_object.write(item + '\n')
+
+file_object.close()
