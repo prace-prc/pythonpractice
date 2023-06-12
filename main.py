d22fffb827cc0746d476032d2ae38683fc4a372a
Juwon Hwang <wndnjs719@gmail.com>
2023-06-12 10:32:13 +0900

    상속 실습

--- main.py
+++ main.py
@@ -1,28 +1,52 @@
-class Car:
-    # 클래스 속성
-    wheel = 4
+# class Car:
+#     # 클래스 속성
+#     wheel = 4
+#
+#     def __init__(self, make, model, color):
+#         self.make = make
+#         self.model = model
+#         self.color = color
+#
+#     def __str__(self):
+#         return f"{self.make}, {self.model}, {self.color}"
+#
+#     # 메소드
+#     def drive(self):
+#         return "The car is moving!"
+#
+#     def stop(self):
+#         return "The car has stopped."
+#
+#
+# my_car = Car("Kia", "Morning", "Blue")
+#
+# # 속성 사용
+# print(my_car.make)
+#
+# # 메소드 호출
+# print(my_car.drive())
+# print(my_car.stop())
 
-    def __init__(self, make, model, color):
+# 상속 실습
+
+# 부모 클래스
+class Vehicle:
+    def __init__(self, make, model, year):
         self.make = make
         self.model = model
-        self.color = color
+        self.year = year
 
-    def __str__(self):
-        return f"{self.make}, {self.model}, {self.color}"
-
-    # 메소드
-    def drive(self):
-        return "The car is moving!"
-
-    def stop(self):
-        return "The car has stopped."
+    def start_engine(self):
+        return "The engine is running!"
 
 
-my_car = Car("Kia", "Morning", "Blue")
+# 자식 클래스
+class Car(Vehicle):
+    def start_engine(self):
+        return super().start_engine() + " It's a car engine."
 
-# 속성 사용
-print(my_car.make)
 
+# 인스턴스 생성
+my_car = Car("Toyota", "Corolla", 2020)
 # 메소드 호출
-print(my_car.drive())
-print(my_car.stop())
+print(my_car.start_engine())
