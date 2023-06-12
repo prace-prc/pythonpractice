208071a948eb47ca5801cd36d35ccd75cddb3370
JM Bae <jungman82@gmail.com>
2023-06-12 10:20:18 +0900

    클래스 실습

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-import os
+class Car:
+    # 클래스 속성
+    wheels = 4
 
-current_directory = os.getcwd()
-print(current_directory)
+    def __init__(self, make, model, color):
+        self.make = make
+        self.model = model
+        self.color = color
 
-# os.mkdir('new_directory')
+    # Method
+    def drive(self):
+        return "The car is moving!"
 
-# os.makedirs('parent_directory/child_directory/grandchild_directory')
+    def stop(self):
+        return f"The car has stopped."
 
-# os.chdir('old_directory')
-# current_directory2 = os.getcwd()
-# print(current_directory2)
 
-# with open('example.txt', 'w') as file_object:
-#     file_object.write('Hello, World!')
+my_car = Car("Kia", "Morning", "Blue")
 
-# os.rename('old_directory', 'new_directory')
+# 속성 사용
+print(my_car.make)
 
-# os.rmdir('new_directory')
-# os.removedirs('parent_directory/child_directory/grandchild_directory')
-
-for dirpath, dirnames, filenames in os.walk('parent_directory'):
-    print(f"디렉터리 경로: {dirpath}")
-    print(f"디렉터리 이름: {dirnames}")
-    print(f"파일 이름: {filenames}")
+# 메소드 호출
+print(my_car.drive())
+print(my_car.stop())
