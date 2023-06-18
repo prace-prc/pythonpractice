9df25959ca5b4a22ddef3b4b232286cd8cc45283
Juwon Hwang <wndnjs719@gmail.com>
2023-06-18 23:49:53 +0900

    6월 18일 백준 풀이

--- 백준 연습장/silver/18111.py
+++ 백준 연습장/silver/18111.py
@@ -1,8 +1,28 @@
 # https://www.acmicpc.net/problem/18111
+import sys
 
-n, m, b = map(int, input().split())
-map = []
+n, m, b = map(int, sys.stdin.readline().split())
+field = []
+fmin, fmax = -1, 0
 
 for i in range(n):
-    map.append(input().split())
+    field.append(list(map(int, sys.stdin.readline().split())))
+    if max(field[i]) > fmax:
+        fmax = max(field[i])
+    if fmin == -1:
+        fmin = min(field[i])
+    if min(field[i]) < fmin:
+        fmin = min(field[i])
+
+print(field, fmax, fmin)
+field_val = {}
+
+for i in field:
+    for j in i:
+        if j not in field_val:
+            field_val[j] = 1
+        else:
+            field_val[j] += 1
+
+print(field_val)
 
