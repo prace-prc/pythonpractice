fe1fd323872fde52872c00dacb4df991e705eacc
JM Bae <jungman82@gmail.com>
2023-06-19 14:16:21 +0900

    urllib request 실습

--- main.py
+++ main.py
@@ -1,33 +1,10 @@
-from collections import deque
-graph = {
-    'A': ['B', 'C'],
-    'B': ['A', 'D', 'E'],
-    'C': ['A', 'F'],
-    'D': ['B'],
-    'E': ['B', 'F'],
-    'F': ['C', 'E']
-}
-visited = set()
-def bfs_iterative(start_node):
-    queue = deque([start_node])
-    while queue:
-        node = queue.popleft()
-        if node not in visited:
-            print(node, end=' ')
-            visited.add(node)
-            queue.extend(graph[node])
-def dfs_iterative(start_node):
-    stack = [start_node]
+import urllib.request
 
-    while stack:
-        node = stack.pop()
-        if node not in visited:
-            print(node, end=' ')
-            visited.add(node)
-            stack.extend(reversed(graph[node]))
+url = 'https://www.google.com'
 
-snode = 'A'
-dfs_iterative(snode)
-visited.clear()
-print('\n-----')
-bfs_iterative(snode)
+request = urllib.request.Request(url)
+response = urllib.request.urlopen(request)
+
+html = response.read()
+
+print(html)
