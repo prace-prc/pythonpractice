2e67b63d5076cdc98c015c96e3f8f2df180e0b6b
Juwon Hwang <wndnjs719@gmail.com>
2023-06-19 15:29:56 +0900

    웹 실습 - 이미지 다운로드 실습

--- main.py
+++ main.py
@@ -26,6 +26,7 @@
 #             slow = slow.next
 #             fast = fast.next.next
 #         return True
+import http.client
 from collections import deque
 
 # https://leetcode.com/problems/number-of-islands/submissions/974400916/
@@ -94,7 +95,7 @@ from collections import deque
 #
 # bfs_iterative(snode)
 
-# 이미지 파서 만들기
+# urllib request
 
 import urllib.request
 
@@ -106,3 +107,78 @@ response = urllib.request.urlopen(request)
 html = response.read()
 
 print(html)
+
+# 이미지 파서 만들기
+
+from urllib.request import urlopen, urlretrieve
+from html.parser import HTMLParser
+from pathlib import Path
+from urllib.parse import urljoin, urlunparse
+
+
+class ImageParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.result = []
+
+    def handle_starttag(self, tag, attrs):
+        if tag != 'img':
+            return
+        if not hasattr(self, 'result'):
+            self.result = []
+        for name, value in attrs:
+            if name == 'src':
+                self.result.append(value)
+
+
+def parse_image(data):
+    parser = ImageParser()
+    parser.feed(data)
+    data_set = set(x for x in parser.result)
+    return data_set
+
+
+# 이미지 다운로드 실습
+def download_image(url, data):
+    download_dir = Path('DOWNLOAD')
+    download_dir.mkdir(exist_ok=True)
+
+    parser = ImageParser()
+    parser.feed(data)
+    data_set = set(x for x in parser.result)
+    for x in sorted(data_set):
+        image_url = urljoin(url, x)
+        basename = Path(image_url).name
+        target_file = download_dir / basename
+        print(target_file)
+
+        print('Downloading...', image_url)
+        urlretrieve(image_url, target_file)
+
+
+def main():
+    # url = 'https://google.co.kr'
+    # with urlopen(url) as f:
+    #     charset = f.headers.get_params('charset')[1][1]
+    #     data = f.read().decode(charset)
+    host = 'www.google.co.kr'
+    conn = http.client.HTTPConnection(host)
+    conn.request('GET', '')
+    resp = conn.getresponse()
+
+    charset = resp.msg.get_param('charset')
+    print('charset:', charset)
+    data = resp.read().decode(charset)
+    conn.close()
+
+    print('\n>>>>> Downloading...', host)
+    url = urlunparse(('http', host, '', '', '', ''))
+
+    # data_set = parse_image(data)
+    # print('\n>>>>> Fetch Images from', url)
+    # print('\n'.join(sorted(data_set)))
+    download_image(url, data)
+
+
+if __name__ == '__main__':
+    main()
