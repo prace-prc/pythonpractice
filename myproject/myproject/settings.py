9d226c4738da3efa2e24d090e92d36738be0c745
Juwon Hwang <wndnjs719@gmail.com>
2023-06-23 09:13:57 +0900

    books 앱 추가

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -29,6 +29,8 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    "books.apps.BooksConfig"
+    # #books",
     "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
