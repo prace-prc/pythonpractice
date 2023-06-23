bc06dc85d0bd8dcfc6933885b03bda856ce60454
Juwon Hwang <wndnjs719@gmail.com>
2023-06-23 14:15:49 +0900

    장고 실습 - 프로젝트 홈페이지 추가

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -41,3 +41,4 @@ def vote(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+
