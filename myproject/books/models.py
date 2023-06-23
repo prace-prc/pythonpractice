cf147906cca1be10ec2696eab94e307832ac9eb2
Juwon Hwang <wndnjs719@gmail.com>
2023-06-23 09:20:46 +0900

    장고 실습 - books 앱 models 수정

--- myproject/books/models.py
+++ myproject/books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField('Author')
+    publisher = models.ForeignKey('Publisher', on_delete=models.CASCADE)
+    publication_date = models.DateField()
+
+    def __str__(self):
+        return self.title
