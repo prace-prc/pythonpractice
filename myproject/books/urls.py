ea2b13bd9ac50f9744f17c2f594b155c4f737dd0
Juwon Hwang <wndnjs719@gmail.com>
2023-06-23 15:42:44 +0900

    장고 실습 - logger 설정, log 파일 git ignore 추가

--- myproject/books/urls.py
+++ myproject/books/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.BooksModelView.as_view(), name='index'),
     path('book/', views.BookList.as_view(), name='book_list'),
     path('author/', views.AuthorList.as_view(), name='author_list'),
-    path('publisher/', views.PublisherList.as_view(), name='publisher_list.html'),
+    path('publisher/', views.PublisherList.as_view(), name='publisher_list'),
 
     path('book/<int:pk>', views.BookDetail.as_view(), name='book_detail'),
     path('author/<int:pk>', views.AuthorDetail.as_view(), name='author_detail'),
