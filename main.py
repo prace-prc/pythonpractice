27c53c21d99364361d7aaa0e2ed21c01a58d4349
Juwon Hwang <wndnjs719@gmail.com>
2023-07-05 13:30:08 +0900

    PostgreSQL 테이블 생성

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
-coffee_menu = {
-    '1': {'name': '에스프레소', 'price': 3000},
-    '2': {'name': '아메리카노', 'price': 4000},
-    '3': {'name': '카페라떼', 'price': 5000},
-    '4': {'name': '카푸치노', 'price': 5000},
-}
+import psycopg2
 
-total_price = 0
+conn = psycopg2.connect(
+    host='localhost',
+    port='5432',
+    dbname='library',
+    user='postgres',
+    password='1234'
+)
 
-def print_menu():
-    print("\n=== 커피 주문 시스템 ===")
-    for id, coffee in coffee_menu.items():
-        print(f"{id}. {coffee['name']} - {coffee['price']}")
+cursor = conn.cursor()
+# cursor.execute('SELECT * FROM Books;')
 
-while True:
-    print_menu()
-    choice = input("원하는 메뉴를 선택하세요: ")
-    if choice in coffee_menu:
-        total_price += coffee_menu[choice]['price']
-        print(f"{coffee_menu[choice]['name']}을(를) 주문하셨습니다.\n 현재까지의 총 금액은 {total_price}원 입니다.")
-    elif choice == '5':
-        print(f"\n주문을 종료합니다. 총 주문금액은 {total_price}원 입니다.")
-        break
-    else:
-        print("\n 잘못된 선택입니다. 다시 선택해주세요.")
+cursor.execute('''
+CREATE TABLE test_table (
+id SERIAL PRIMARY KEY,
+name VARCHAR(50))
+''')
+
+conn.commit()
+
+cursor.close()
+conn.close()
