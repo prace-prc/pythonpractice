221417b5e6a34d8d57a7bff14989574a2de8a180
Juwon Hwang <wndnjs719@gmail.com>
2023-07-05 13:46:33 +0900

    PostgreSQL insert, update, select

--- main.py
+++ main.py
@@ -9,15 +9,25 @@ conn = psycopg2.connect(
 )
 
 cursor = conn.cursor()
-# cursor.execute('SELECT * FROM Books;')
 
-cursor.execute('''
-CREATE TABLE test_table (
-id SERIAL PRIMARY KEY,
-name VARCHAR(50))
-''')
+# cursor.execute('''
+# CREATE TABLE test_table (
+# id SERIAL PRIMARY KEY,
+# name VARCHAR(50))
+# ''')
+# conn.commit()
 
+# cursor.execute("INSERT INTO test_table (name) VALUES (%s)", ("Test name",))
+# conn.commit()
+
+cursor.execute("update test_table set name = 'Change' where id = 1")
 conn.commit()
+
+cursor.execute('SELECT * FROM test_table;')
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
 
 cursor.close()
 conn.close()
